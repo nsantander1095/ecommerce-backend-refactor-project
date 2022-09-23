2968d7a024f9928f64314d7273e7fdd7b46849c3
Nick <nsantander1095@gmail.com>
2022-09-23 18:12:17 -0400

    fixed the delete route for categories based on what we did in class office hours

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -54,6 +54,7 @@ router.put('/:id', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  await models.Product.destroy({ where: { category_id: req.params.id }})
   const categoryData = await models.Category.destroy({
     where: {
       id: req.params.id
